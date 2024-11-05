eec45409fe9918928ab9b153e698edfafd981c3e
June <junetiger10@naver.com>
2024-11-05 18:25:02 +0900

    react 맛보기

---
+++ src/practice/App1.jsx
@@ -0,0 +1,15 @@
+function App1() {
+  return (
+    <div>
+      <h1>Loream.</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <button>버튼</button>
+    </div>
+  );
+}
+
+export default App1();
