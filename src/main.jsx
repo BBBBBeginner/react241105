4f6b66483bce143a3f560bfc40b9e79c76734401
June <junetiger10@naver.com>
2024-11-06 16:49:36 +0900

    chakra ui 맛보기

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp2 from "./apps/MyApp2.jsx";
-import App2 from "./practice/App2.jsx";
+import App24 from "./practice/App24.jsx";
+import { Provider } from "./components/ui/provider.jsx";
+import MyApp25 from "./apps/MyApp25.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp2 />
-    <App2 />
+    <Provider>
+      <App24 />
+      <MyApp25 />
+    </Provider>
   </StrictMode>,
 );
