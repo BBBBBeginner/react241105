14c3700436ae69a77efebee952b37c749d866e4a
June <junetiger10@naver.com>
2024-11-06 16:46:53 +0900

    style prop

---
+++ src/apps/MyApp23.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function MyApp23(props) {
+  return (
+    <div>
+      <h1
+        style={{
+          color: "yellow",
+          backgroundColor: "black",
+        }}
+      >
+        Lorem3
+      </h1>
+      <p
+        style={{
+          paddingLeft: "50px",
+          border: "2px solid green",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+        ducimus minus nihil nostrum quas sapiente voluptas voluptatum!
+        Asperiores assumenda eius enim fuga iure minus nesciunt nostrum
+        similique, unde vel! Necessitatibus.
+      </p>
+      <ul
+        style={{
+          listStyleType: "space-counter",
+          color: "blue",
+        }}
+      >
+        <li>lorem</li>
+        <li>indpd!</li>
+        <li>aeicfim</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyApp23;
